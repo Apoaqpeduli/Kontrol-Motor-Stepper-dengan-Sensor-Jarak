f189847a4bcae62c3e554d9b632f985e3a08268a
Azhari Bastomi <63101438+Apoaqpeduli@users.noreply.github.com>
2023-07-14 22:09:14 +0700

    update

--- Program/check_motor_stepper/check_motor_stepper.ino
+++ Program/check_motor_stepper/check_motor_stepper.ino
@@ -1,11 +1,11 @@
-#include <AccelStepper.h> 
+#include <AccelStepper.h>
 
-#define MP1  8 
+#define MP1  8
 #define MP2  9
 #define MP3  10
 #define MP4  11
 
-#define MotorInterfaceType 8 
+#define MotorInterfaceType 8
 AccelStepper stepper = AccelStepper(MotorInterfaceType, MP1, MP3, MP2, MP4);
 const int SPR = 2048;//360
 //const int SPR = 1024;//180
@@ -14,15 +14,14 @@ const int SPR = 2048;//360
 //const int SPR = 128;//22.5
 
 void setup() {
-  stepper.setMaxSpeed(1000);//kecepatan maximum 
+  stepper.setMaxSpeed(1000);//kecepatan maximum
   stepper.setAcceleration(200);//Set the maximum acceleration in steps per second^2
 }
 
 void loop() {
-  stepper.moveTo(SPR); 
-  stepper.runToPosition();
-  delay(1000);
-  stepper.moveTo(SPR);
-  stepper.runToPosition();
-  delay(1000);
+  for (int a = 0; a <= 100; a ? ++) {
+    stepper.moveTo(SPR);
+    stepper.runToPosition();
+    delay(1000);
+  }
 }
